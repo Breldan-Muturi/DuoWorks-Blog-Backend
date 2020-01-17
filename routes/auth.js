30406c1ee88105ac3c1b58edcb66c0c327d1047c
Turi-Byte <breldanmuturi001@gmail.com>
2020-01-17 14:19:34 +0300

    Add Reset password account activation and google social login

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,33 @@
 const express = require('express');
 const router = express.Router();
-const {signup, signin, signout, requireSignin} = require('../controllers/auth');
+const {
+    signup,
+    signin,
+    signout,
+    requireSignin,
+    forgotPassword,
+    resetPassword,
+    preSignup,
+    googleLogin
+} = require('../controllers/auth');
 
 // validators
 const {runValidation} = require('../validators');
-const {userSignupValidator, userSigninValidator} = require('../validators/auth');
+const {
+    userSignupValidator,
+    userSigninValidator,
+    forgotPasswordValidator,
+    resetPasswordValidator
+} = require('../validators/auth');
 
-router.post('/signup', userSignupValidator, runValidation, signup);  //We apply our validators here as middleware so only if the validation is passed is the code in the sign up method executed
+router.post('/pre-signup', userSignupValidator, runValidation, preSignup);
+router.post('/signup', signup);  //We apply our validators here as middleware so only if the validation is passed is the code in the sign up method executed
 router.post('/signin', userSigninValidator, runValidation, signin); 
 router.get('/signout', signout);
+router.put('/forgot-password', forgotPasswordValidator, runValidation, forgotPassword);
+router.put('/reset-password', resetPasswordValidator, runValidation, resetPassword);
+// google login
+router.post('/google-login', googleLogin);
 
 //test
 // router.get('/secret', requireSignin, authMiddleware, (req, res) => {
