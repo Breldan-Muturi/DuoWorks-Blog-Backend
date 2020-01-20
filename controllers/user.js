7710dcdb225ec4f861a9c4380f31489b09959c2e
Turi-Byte <breldanmuturi001@gmail.com>
2020-01-20 12:44:15 +0300

    Add a few items onto the list

--- controllers/user.js
+++ controllers/user.js
@@ -45,36 +45,55 @@ exports.publicProfile = (req,res) => {
     });
 };
 
-exports.update = (req,res) => {
+exports.update = (req, res) => {
     let form = new formidable.IncomingForm();
-    form.keepExtensions = true;
+    form.keepExtension = true;
     form.parse(req, (err, fields, files) => {
-        if(err){
+        if (err) {
             return res.status(400).json({
-                error: 'Photo could not be loaded'
+                error: 'Photo could not be uploaded'
             });
         }
-        let user = req.profile; 
-        user= _.extend(user,fields);
-
-        if(fields.password && fields.password.length < 6){
+ 
+        let user = req.profile;
+        // user's existing role and email before update
+        let existingRole = user.role;
+        let existingEmail = user.email;
+ 
+        if (fields && fields.username && fields.username.length > 12) {
+            return res.status(400).json({
+                error: 'Username should be less than 12 characters long'
+            });
+        }
+ 
+        if (fields.username) {
+            fields.username = slugify(fields.username).toLowerCase();
+        }
+ 
+        if (fields.password && fields.password.length < 6) {
             return res.status(400).json({
                 error: 'Password should be min 6 characters long'
             });
         }
-
-        if(files.photo){
-            if(files.photo.size > 1000000) {
+ 
+        user = _.extend(user, fields);
+        // user's existing role and email - dont update - keep it same
+        user.role = existingRole;
+        user.email = existingEmail;
+ 
+        if (files.photo) {
+            if (files.photo.size > 10000000) {
                 return res.status(400).json({
-                    error: 'Image should be less than 1 mb'
+                    error: 'Image should be less than 1mb'
                 });
             }
             user.photo.data = fs.readFileSync(files.photo.path);
             user.photo.contentType = files.photo.type;
         }
-            
+ 
         user.save((err, result) => {
-            if(err) {
+            if (err) {
+                console.log('profile udpate error', err);
                 return res.status(400).json({
                     error: errorHandler(err)
                 });
